c29172d46f9f3d2e9efb2d97f4134444e764ab0c
Stanislav Dashkov <stdashkov@mail.ru>
2017-10-27 21:17:12 +0300

    add body-parser & create simple add friends

--- app.js
+++ app.js
@@ -1,21 +1,25 @@
 var express = require("express");
 var app = express();
+var bodyParser = require('body-parser');
 
+app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
+
+var friends = ["Tony", "LA", "LD", "LD", "F","D"]
 
 app.get("/", function(req, res){
   res.render("home");
 });
 
 app.post("/addfriend", function(req, res){
-  res.send("You have reached ");
+  var newFriend = req.body.newfriend;
+  friends.push(newFriend);
+  res.redirect("/friends");
 });
 
 app.get("/friends", function(req, res){
-  var friends = ["Tony", "LA", "LD", "LD", "F","D"]
   res.render("friends", {friends: friends});
 });
-
 
 app.listen(process.env.PORT, process.env.OP, function(){
   console.log("Server starRt");
