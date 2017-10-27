3a9bd61507f5989ed7c08a3bca4bdc1ec975ef9b
Stanislav Dashkov <stdashkov@mail.ru>
2017-10-27 20:54:42 +0300

    run server & add button friends

--- app.js
+++ app.js
@@ -1,12 +1,38 @@
-var express = require('express');
+var express = require("express");
 var app = express();
 
-app.set("view engine", "enj");
+app.set("view engine", "ejs");
 
 app.get("/", function(req, res){
-  res.render('home');
+  res.render("home");
 });
+
+app.post("/addfriend", function(req, res){
+  res.send("You have reached ");
+});
+
+app.get("/friends", function(req, res){
+  var friends = ["Tony", "LA", "LD", "LD", "F","D"]
+  res.render("friends", {friends: friends});
+});
+
 
 app.listen(process.env.PORT, process.env.OP, function(){
   console.log("Server starRt");
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+app.listen(3000);
