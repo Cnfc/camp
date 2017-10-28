29cd46722946969346c6efbb866b41f3808fa867
Stanislav Dashkov <stdashkov@mail.ru>
2017-10-28 08:49:19 +0300

    add /new(add camp) and <= =>

--- app.js
+++ app.js
@@ -5,7 +5,12 @@ var bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 
-// var friends = ["Tony", "LA", "LD", "LD", "F","D"]
+var campgrounds = [
+  {name: "Salmon Creak", image: "https://farm1.staticflickr.com/130/321487195_ff34bde2f5.jpg"},
+  {name: "Grantile Hill", image: "https://farm1.staticflickr.com/130/321487195_ff34bde2f5.jpg"},
+  {name: "Mountain", image: "https://farm1.staticflickr.com/130/321487195_ff34bde2f5.jpg"},
+  {name: "Mountain", image: "https://farm1.staticflickr.com/130/321487195_ff34bde2f5.jpg"}
+];
 
 app.get("/", function(req, res){
   res.render("landing");
@@ -19,13 +24,21 @@ app.get("/", function(req, res){
 //
 
 app.get("/campgrounds", function(req, res){
-  var campgrounds = [
-    {name: "Salmon Creak", image: "https://farm1.staticflickr.com/130/321487195_ff34bde2f5.jpg"},
-    {name: "Grantile Hill", image: "https://farm1.staticflickr.com/130/321487195_ff34bde2f5.jpg"},
-    {name: "Mountain", image: "https://farm1.staticflickr.com/130/321487195_ff34bde2f5.jpg"},
-    {name: "Mountain", image: "https://farm1.staticflickr.com/130/321487195_ff34bde2f5.jpg"}
-  ]
+
   res.render("campgrounds", {campgrounds: campgrounds});
+});
+
+app.get("/campgrounds/new", function(req, res){
+  res.render("new.ejs");
+});
+
+app.post("/campgrounds", function(req, res){
+  var name = req.body.name;
+  var image = req.body.image;
+  var newCampground = {name: name, image: image};
+  campgrounds.push(newCampground);
+  //redirect back to campgrounds page
+  res.redirect("/campgrounds");
 });
 
 app.listen(process.env.PORT, process.env.OP, function(){
