335a4990bd7cde8db52731c3597eef5dcb173ac1
Stanislav Dashkov <stdashkov@mail.ru>
2017-10-31 17:11:06 +0300

    add users register(sing up)

--- app.js
+++ app.js
@@ -9,13 +9,25 @@ var express    = require("express"),
     User       = require("./models/user"),
     seedDB     = require("./seeds")
 
-mongoose.connect("mongodb://localhost/yelp_camp_v4");
+mongoose.connect("mongodb://localhost/yelp_camp_v6");
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
 console.log(__dirname);
 seedDB();
 
+
+// PASSPORT CONFIG
+app.use(require("express-session")({
+  secret: "Once",
+  resave: false,
+  saveUninitialized: false
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 
 app.get("/", function(req, res){
   res.render("landing");
@@ -124,7 +136,29 @@ app.post("/campgrounds/:id/comments", function(req, res){
   // COnnect new comments ro campground
   // redirect campground
 });
+//  ========================================================
+// AUTH ROUTES
+//=============================================================
 
+//show register form
+app.get("/register", function(req, res){
+  res.render("register");
+});
+// handle sing up logic
+app.post("/register", function(req, res){
+  var newUser = new User({username: req.body.username});
+  User.register(newUser, req.body.password, function(err, user){
+    if(err){
+      console.log(err);
+      return res.render("register");
+    } else {
+      passport.authenticate("local")(req, res, function(){
+        res.redirect("/campgrounds");
+      });
+    };
+  });
+
+});
 
 app.listen(process.env.PORT, process.env.IP, function(){
   console.log("The YalpCamp Has starRt");
