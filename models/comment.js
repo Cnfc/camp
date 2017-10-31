64424cf75a2a5a786f3186efa9710c884a39183a
Stanislav Dashkov <stdashkov@mail.ru>
2017-10-31 21:05:45 +0300

    Save authors name to comment automatically

--- models/comment.js
+++ models/comment.js
@@ -3,7 +3,13 @@ var mongoose = require("mongoose");
 
 var commentSchema = mongoose.Schema({
   text: String,
-  author: String
+  author: {
+    id: {
+      type: mongoose.Schema.Types.ObjectId,
+      reg: "User"
+    },
+    username: String
+  }
 });
 
 module.exports = mongoose.model("Comment", commentSchema);
